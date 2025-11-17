bd54c56528203b8921049af204c15871b723b23c
ank-singhal <ankit.singhal@frequency.com>
2025-11-17 15:10:06 +0530

    completion of data type assignment

---
+++ data-type.py
@@ -0,0 +1,33 @@
+
+# Task 1
+# Converting float to int and print the value
+print(int(3.75))
+
+# Converting string to int and print the value
+print(int("123"))
+
+
+# Converting int to boolean and print the value
+print(bool(0))
+
+# Converting boolean to string and print the value
+print(f"{False}")
+
+# Converting the string to upper case
+print("hello".upper())
+
+# addition of numbers and determine type of sum and then type conversion
+x= 5
+y = 3.14
+z = x + y
+print(type(z))
+print(f"sum after conversion is {int(z)}")
+
+
+# perform operation on string
+s = 'hello'
+print(s.upper())
+new_s = s.replace('e', 'a')
+print(new_s)
+print(s.startswith('he'))
+print(s.endswith('lo'))
