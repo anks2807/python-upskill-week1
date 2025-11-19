99fdd4d0cf9c1b4ca199347393c9bbd6be1ae7ef
ank-singhal <ankit.singhal@frequency.com>
2025-11-19 14:39:47 +0530

    control flow assignment

---
+++ check-day-of-week.py
@@ -0,0 +1,9 @@
+day = input("Enter a day of the week: ").strip().lower()
+
+match day:
+    case "monday" | "tuesday" | "wednesday" | "thursday" | "friday":
+        print("Weekday")
+    case "saturday" | "sunday":
+        print("Weekend")
+    case _:
+        print("Invalid day")
--- check-even-odd.py
+++ check-even-odd.py
@@ -1,6 +1,27 @@
+#determine the input number is even or odd
 num = int(input("Enter a number: "))
 
 if num % 2 == 0:
     print(f"{num} is an even number.")
 else:
     print(f"{num} is an odd number.")
+
+
+# Find 2 numbers where result is equal to the target number
+numbers = [1, 2, 3, 4, 5]
+target = 9
+
+for n in numbers:
+    diff = target - n
+    if diff in numbers and diff != n:
+        print(f"Numbers found: {[n, diff]}")
+        break
+
+# print fibonacci series up to n terms
+n_terms = int(input("Enter number of terms for Fibonacci series: "))
+fib = [0, 1]
+for i in range(2, n_terms):
+    next_term = fib[i-1] + fib[i-2]
+    fib.append(next_term)
+
+print(f"Fibonacci seq for {n_terms} is {fib}")
